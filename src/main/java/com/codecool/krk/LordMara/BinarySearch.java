9591ef1d9978c52a8fb7186b376818afe46b1859
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 16:32:15 +0100

    Implement doNotRecursiveBinarySearch() method

--- src/main/java/com/codecool/krk/LordMara/BinarySearch.java
+++ src/main/java/com/codecool/krk/LordMara/BinarySearch.java
@@ -1,6 +1,7 @@
 package com.codecool.krk.LordMara;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class BinarySearch {
     public boolean doRecursiveBinarySearch(int[] listOfIntegers, int searchValue) {
@@ -16,5 +17,27 @@ public class BinarySearch {
         } else {
             return doRecursiveBinarySearch(Arrays.copyOfRange(listOfIntegers, midpoint, listOfIntegers.length), searchValue);
         }
+    }
+
+    public boolean doNotRecursiveBinarySearch(List<Integer> listOfIntegers, int searchValue) {
+        if (listOfIntegers.size() == 0) {
+            return false;
+        }
+
+        int first = 0;
+        int last = listOfIntegers.size() - 1;
+
+        while (first <= last) {
+            int midpoint = first + last / 2;
+
+            if (listOfIntegers.get(midpoint).equals(searchValue)) {
+                return true;
+            } else if (searchValue < listOfIntegers.get(midpoint)) {
+                last = midpoint - 1;
+            } else {
+                first = midpoint + 1;
+            }
+        }
+        return false;
     }
 }
