d6f6b6f11cd351b8a1d361efe3958f44a63e6047
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 16:31:36 +0100

    Implement doRecursiveBinarySearch() method

--- src/main/java/com/codecool/krk/LordMara/BinarySearch.java
+++ src/main/java/com/codecool/krk/LordMara/BinarySearch.java
@@ -1,5 +1,20 @@
 package com.codecool.krk.LordMara;
 
-public class BinarySearch {
+import java.util.Arrays;
 
+public class BinarySearch {
+    public boolean doRecursiveBinarySearch(int[] listOfIntegers, int searchValue) {
+        if (listOfIntegers.length == 0) {
+            return false;
+        }
+
+        int midpoint = listOfIntegers.length / 2;
+        if (listOfIntegers[midpoint] == searchValue) {
+            return true;
+        } else if (searchValue < listOfIntegers[midpoint]) {
+            return doRecursiveBinarySearch(Arrays.copyOfRange(listOfIntegers, 0, midpoint), searchValue);
+        } else {
+            return doRecursiveBinarySearch(Arrays.copyOfRange(listOfIntegers, midpoint, listOfIntegers.length), searchValue);
+        }
+    }
 }
